4b1fcb20014aadc76a23eb5e1a73122ae6d0ce4f
Harry G. Thompson <harygthompson@postoe.net>
2026-09-22 01:46:32 +0100

    Update .config/doom/config.el

--- private_dot_config/doom/config.el
+++ private_dot_config/doom/config.el
@@ -33,11 +33,12 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;;(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-tokyo-night)
 ;; Specify both a dark and light theme, like so and Doom will choose which one
 ;; to load based on your system light/dark setting:
 ;;
 ;;   (setq doom-theme '(doom-one   . doom-one-light))   ; (DARK . LIGHT)
-(setq doom-theme '(doom-tokyo-night   . doom-one-light))   ; (DARK . LIGHT)
+
 ;; If you want more pro-active theme switching based on OS light/dark mode, look
 ;; up the `auto-dark' package.
 
